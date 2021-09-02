adf85be900aae39314333ae5d6fc10594eb81072
anovinc <67967868+anovinc@users.noreply.github.com>
2021-09-02 19:02:04 +0200

    new service implemented (#33)

---
+++ php/new_service.php
@@ -0,0 +1,52 @@
+<?php
+
+    if (!isset($_COOKIE['Admin'])) {
+        header("Location: admin.php");
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html land="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" Href="../style/style.css">
+        <title>New service</title>
+    </head>
+
+    <body class="dashboard">
+        <div class="sidenav">
+            <a href="../index.php"><span>Used Electronics</span></a>    
+            <a href="./dashboard.php">Dashboard</a>
+            <a href="" style="font-size: 20px;"><b>Services</b></a>
+            <a href="./items.php" >Items</a>
+            <a href="./logout.php">Logout</a>
+        </div>
+
+        <section class="details">
+            <div>
+                <h1>New service</h1>
+                <span>New service</span>
+            </div>
+            
+            <form class="add_service" action="validate_new_service.php" method="post">
+                <p>
+                    <label for="text">Name: </label>
+                    <input class="textinput" type="text" name="service_name" id="service_name" 
+                        placeholder="Name">
+                </p>
+                <p>
+                    <label for="number">Price: </label>
+                    <input class="textinput" type="number" name="service_price" id="service_price"
+                        placeholder="Price">
+                </p>
+
+                <input class="addbtn" type="submit" value="Add new service">
+        
+            </form>
+        </section>
+    </body>
+</html>
