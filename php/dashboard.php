b0c6e6d719f7796d4c82ff640960e2f797cc5fde
David Turkalj <davidturkalj@pop-os.localdomain>
2021-09-02 20:35:17 +0200

    Implement dashboard

---
+++ php/dashboard.php
@@ -0,0 +1,35 @@
+<?php
+    if(!isset($_COOKIE['Admin'])){
+        header("Location: admin.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html land="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../style/style.css">
+        <title>Dashboard</title>
+    </head>
+
+    <body class="dashboard">
+        <div class="sidenav">
+            <a href="../index.php"><span>Used Electronics</span></a>    
+            <a href="" style="font-size: 20px;"><b>Dashboard</b></a>
+            <a href="./services.php">Services</a>
+            <a href="./items.php">Items</a>
+            <a href="./logout.php">Logout</a>
+        </div>
+                
+        <div class="details">
+            <span>User info</span>
+            <p class="info">
+                <?php echo "Admin profil:  " . $_COOKIE["Admin"]; ?><br>
+            </p>
+        </div>
+    </body>
+
+</html>
