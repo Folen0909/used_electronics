eaee1cd45090d2b214d25faa10ea936234f75492
David Turkalj <davidturkalj@pop-os.localdomain>
2021-09-03 20:49:36 +0200

    Switched to external db

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 
     final class DBConfig {
-        const HOST = 'db';
-        const DB_NAME = 'ferit';
-        const USERNAME = 'user';
-        const PASS = 'test';
+        const HOST = 'ec2-34-249-247-7.eu-west-1.compute.amazonaws.com';
+        const DB_NAME = 'dftn6439l34u5m';
+        const USERNAME = 'hsklcrpuovlitf';
+        const PASS = '781161d8d06cfea1b016b382fcd1717f9816ea4cfef52534dd2cfd22fd5caf66';
     }
 
 ?>
