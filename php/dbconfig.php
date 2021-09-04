469178627a73fc4f4b91057db9435d2c73650f43
David Turkalj <davidturkalj@pop-os.localdomain>
2021-09-04 18:37:03 +0200

    Switched db

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 
     final class DBConfig {
-        const HOST = 'ec2-34-249-247-7.eu-west-1.compute.amazonaws.com';
-        const DB_NAME = 'dftn6439l34u5m';
-        const USERNAME = 'hsklcrpuovlitf';
-        const PASS = '781161d8d06cfea1b016b382fcd1717f9816ea4cfef52534dd2cfd22fd5caf66';
+        const HOST = 'sql11.freesqldatabase.com';
+        const DB_NAME = 'sql11434588';
+        const USERNAME = 'sql11434588';
+        const PASS = 'vFvMSxLV1G';
     }
 
 ?>
